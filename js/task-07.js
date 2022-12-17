411f9d4e381ffddf33fccc7d9a57300d20ae0724
vvv-k <vkorotlov@gmail.com>
2022-12-17 08:20:32 +0300

    first commit

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const fontSizeControlEl = document.getElementById('font-size-control');
+const text = document.getElementById('text');
+
+fontSizeControlEl.addEventListener('input', () => {
+    text.style.fontSize = fontSizeControlEl.value + "px";
+})
+
+
